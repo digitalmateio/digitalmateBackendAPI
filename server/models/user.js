ba40e7bc10df08af3ff5e50a8023cf27e44ab4d0
coreycosman <cosman.corey@gmail.com>
2019-02-13 21:28:37 +0400

    joi validation on user model with custom error messages

--- server/models/user.js
+++ server/models/user.js
@@ -1,52 +1,91 @@
 // USER MODEL:
-
+// NOTE: use const for everything here for readablity
+// NOTE: add other mongoose elements to joi schema
+// NOTE: confirmation doesnt need to be stored - can be checked on frontend, research this
 const mongoose = require('mongoose'),
   Joi = require('joi'),
   email = Joi.string()
     .email()
+    .required()
     .lowercase()
     .trim()
     .error(errors => {
-      errors.message = 'test';
-      return errors.message;
+      const emailErrorsArray = errors.map(err => {
+        switch (err.type) {
+          case 'any.empty':
+            return { message: 'email must be present' };
+          case 'string.email':
+            return { message: 'invalid email format' };
+          default:
+            return;
+        }
+      });
+      // remove duplicates in email errors array
+      // NOTE: export as helper function
+      const uniqueErrors = (array, prop) => {
+        let uniqueArray = [];
+        let uniqueErrorsObj = {};
+
+        array.forEach(error => {
+          uniqueErrorsObj[error[prop]] = error;
+        });
+
+        for (errorKey in uniqueErrorsObj) {
+          uniqueArray.push(uniqueErrorsObj[errorKey]);
+        }
+        return uniqueArray;
+      };
+      return uniqueErrors(emailErrorsArray, 'message');
     }),
-  // }),
-  // .error(errors => {
-  //   return (errors.message = 'test');
-  // }),
-  // .error(errors => {
-  //   return {
-  //     message: 'test',
-  //   };
-  // }),
   password = Joi.string()
     .min(8)
-    .error(() => 'invalid password format'),
+    .error(errors => {
+      const passwordErrorsArray = errors.map(err => {
+        switch (err.type) {
+          case 'any.empty':
+            return { message: 'password must be present' };
+          case 'string.min':
+            return { message: 'password must be at least 8 characters' };
+          default:
+            return;
+        }
+      });
+      return passwordErrorsArray;
+    }),
   confirmation = Joi.string()
     .min(8)
-    .error(() => 'invalid password confirmation'),
-  Joigoose = require('joigoose')(mongoose);
-
-const joiUserSchema = Joi.object()
-  // .options({ abortEarly: false })
-  .keys({
-    email: email.required(),
-    password: password.required(),
-    confirmation: confirmation.required(),
-  })
-  .required();
-
-const validationSchemas = {
-  signup: Joi.object()
+    .error(errors => {
+      const confirmationErrorsArray = errors.map(err => {
+        switch (err.type) {
+          case 'any.empty':
+            return { message: 'confirmation must be present' };
+          case 'string.min':
+            return { message: 'confirmation must be at least 8 characters' };
+          default:
+            return;
+        }
+      });
+      return confirmationErrorsArray;
+    }),
+  Joigoose = require('joigoose')(mongoose),
+  joiUserSchema = Joi.object()
     // .options({ abortEarly: false })
     .keys({
       email: email.required(),
       password: password.required(),
+      confirmation: confirmation.required(),
     })
     .required(),
-};
-
-const UserSchema = new mongoose.Schema(Joigoose.convert(joiUserSchema));
+  validationSchemas = {
+    signup: Joi.object()
+      .options({ abortEarly: false })
+      .keys({
+        email,
+        password: password.required(),
+      })
+      .required(),
+  },
+  UserSchema = new mongoose.Schema(Joigoose.convert(joiUserSchema));
 
 // const UserSchema = new mongoose.Schema({
 //   email: {
@@ -93,9 +132,13 @@ module.exports = {
   User,
   validate: schemaType => (req, res, next) => {
     const result = Joi.validate(req.body, validationSchemas[schemaType]);
+    let errorsArray = [];
     if (result.error) {
+      result.error.details.forEach(err => {
+        errorsArray.push(err.message);
+      });
       return res.status(400).json({
-        error: result.error.message,
+        error: errorsArray,
       });
     }
     if (!req.value) req.value = {};
